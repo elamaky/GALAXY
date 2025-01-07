ec212b8f55deaf657932b9e126a78d13f87eea37
elamaky <nemirnaela@gmail.com>
2025-01-07 22:31:44 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,5 +6,5 @@ sed -i "s/<source-password>[^<]*<\/source-password>/<source-password>galaxy<\/so
 sed -i "s/<relay-password>[^<]*<\/relay-password>/<relay-password>galaxy<\/relay-password>/g" /etc/icecast2/icecast.xml
 sed -i "s/<admin-password>[^<]*<\/admin-password>/<admin-password>galaxy<\/admin-password>/g" /etc/icecast2/icecast.xml
 
-# Pokrećemo Icecast server
-exec sudo -Eu icecast2 icecast2 -n -c /etc/icecast2/icecast.xml
+# Pokrećemo Icecast server direktno bez sudo, kao icecast2 korisnik
+exec icecast2 -n -c /etc/icecast2/icecast.xml
