9519582096d76e6df4bc3a93caccf8450bc9f04a
elamaky <nemirnaela@gmail.com>
2025-01-07 20:29:39 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,26 +1,33 @@
 #!/bin/sh
 
+# Prikazivanje svih promenljivih okruženja
 env
 
+# Omogućavanje režima za praćenje izvršavanja skripte
 set -x
 
+# Funkcija za postavljanje vrednosti u icecast.xml
 set_val() {
     if [ -n "$2" ]; then
         echo "set '$2' to '$1'"
-        sed -i "s/<$2>[^<]*<\/$2>/<$2>$1<\/$2>/g" /etc/icecast2/icecast.xml
+        # Ažuriranje vrednosti u icecast.xml koristeći sed
+        sed -i "s|<$2>[^<]*</$2>|<$2>$1</$2>|g" /etc/icecast2/icecast.xml
     else
         echo "Setting for '$1' is missing, skipping." >&2
     fi
 }
 
-set_val $ICECAST_SOURCE_PASSWORD source-galaxy
-set_val $ICECAST_RELAY_PASSWORD  relay-galaxy
-set_val $ICECAST_ADMIN_PASSWORD  admin-galaxy
-set_val $ICECAST_PASSWORD        galaxy
-set_val $ICECAST_HOSTNAME        https://galaxy-mn11.onrender.com
+# Postavljanje vrednosti za Icecast konfiguraciju
+set_val "$ICECAST_SOURCE_PASSWORD" source-password
+set_val "$ICECAST_RELAY_PASSWORD" relay-password
+set_val "$ICECAST_ADMIN_PASSWORD" admin-password
+set_val "$ICECAST_PASSWORD" password
+set_val "$ICECAST_HOSTNAME" hostname
 
+# Zaustavljanje skripte u slučaju greške
 set -e
 
+# Pokretanje Icecast servera sa sudo i icecast2 korisnikom
 sudo -Eu icecast2 icecast2 -n -c /etc/icecast2/icecast.xml
 
 # Pokrećemo Icecast server
