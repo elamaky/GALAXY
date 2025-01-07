ce7c4b7b15141fdb1df6d0a635aa9a3d0e231d69
elamaky <nemirnaela@gmail.com>
2025-01-07 21:24:40 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -8,12 +8,12 @@ set -x
 
 # Funkcija za postavljanje vrednosti u icecast.xml
 set_val() {
-    if [ -n "$2" ]; then
+    if [ -n "$1" ]; then
         echo "set '$2' to '$1'"
         # Ažuriranje vrednosti u icecast.xml koristeći sed
         sed -i "s|<$2>[^<]*</$2>|<$2>$1</$2>|g" /etc/icecast2/icecast.xml
     else
-        echo "Setting for '$1' is missing, skipping." >&2
+        echo "Setting for '$2' is missing, skipping." >&2
     fi
 }
 
@@ -27,8 +27,8 @@ set_val "$ICECAST_HOSTNAME" hostname
 # Zaustavljanje skripte u slučaju greške
 set -e
 
-# Pokretanje Icecast servera sa sudo i icecast2 korisnikom
-sudo -Eu icecast2 icecast2 -n -c /etc/icecast2/icecast.xml
+# Pokretanje Icecast servera bez sudo komande
+icecast2 -n -c /etc/icecast2/icecast.xml
 
 # Pokrećemo Icecast server
-exec sudo -Eu icecast2 icecast2 -n -c /etc/icecast2/icecast.xml
+exec icecast2 -n -c /etc/icecast2/icecast.xml
