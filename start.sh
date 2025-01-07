53483b45c20216657741bebb21270a3665e69c70
elamaky <nemirnaela@gmail.com>
2025-01-07 22:13:22 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,32 +1,10 @@
-#!/bin/sh
+#!/bin/bash
 
-# Prikazivanje svih promenljivih okruženja
-env
+# Ažuriramo konfiguracioni fajl sa odgovarajućim vrednostima
+sed -i "s/<hostname>[^<]*<\/hostname>/<hostname>${ICECAST_HOSTNAME}<\/hostname>/g" /etc/icecast2/icecast.xml
+sed -i "s/<source-password>[^<]*<\/source-password>/<source-password>galaxy<\/source-password>/g" /etc/icecast2/icecast.xml
+sed -i "s/<relay-password>[^<]*<\/relay-password>/<relay-password>galaxy<\/relay-password>/g" /etc/icecast2/icecast.xml
+sed -i "s/<admin-password>[^<]*<\/admin-password>/<admin-password>galaxy<\/admin-password>/g" /etc/icecast2/icecast.xml
 
-# Omogućavanje režima za praćenje izvršavanja skripte
-set -x
-
-# Funkcija za postavljanje vrednosti u icecast.xml
-set_val() {
-    if [ -n "$2" ]; then
-        echo "set '$2' to '$1'"
-        # Ažuriranje vrednosti u icecast.xml koristeći sed
-        sed -i "s|<$2>[^<]*</$2>|<$2>$1</$2>|g" /etc/icecast2/icecast.xml
-    else
-        echo "Setting for '$1' is missing, skipping." >&2
-    fi
-}
-
-# Postavljanje vrednosti za Icecast konfiguraciju
-set_val "galaxy" source-password
-set_val "galaxy" relay-password
-set_val "angel" admin-password
-set_val "galaxy" password
-set_val "noise.example.com" hostname
-
-# Zaustavljanje skripte u slučaju greške
-set -e
-
-# Pokretanje Icecast servera sa sudo i icecast2 korisnikom
+# Pokrećemo Icecast server
 exec sudo -Eu icecast2 icecast2 -n -c /etc/icecast2/icecast.xml
-
