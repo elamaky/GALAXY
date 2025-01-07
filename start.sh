7acb7982f8a60fa1f5df6dc147756847c33a3c62
elamaky <nemirnaela@gmail.com>
2025-01-07 11:40:41 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,9 +2,9 @@
 
 # Ažuriramo konfiguracioni fajl sa odgovarajućim vrednostima
 sed -i "s/<hostname>[^<]*<\/hostname>/<hostname>${ICECAST_HOSTNAME}<\/hostname>/g" /etc/icecast2/icecast.xml
-sed -i "s/<source-password>[^<]*<\/source-password>/<source-password>galaxy<\/source-password>/g" /etc/icecast2/icecast.xml
-sed -i "s/<relay-password>[^<]*<\/relay-password>/<relay-password>galaxy<\/relay-password>/g" /etc/icecast2/icecast.xml
-sed -i "s/<admin-password>[^<]*<\/admin-password>/<admin-password>galaxy<\/admin-password>/g" /etc/icecast2/icecast.xml
+sed -i "s/<source-password>[^<]*<\/source-password>/<source-password>${ICECAST_SOURCE_PASSWORD}<\/source-password>/g" /etc/icecast2/icecast.xml
+sed -i "s/<relay-password>[^<]*<\/relay-password>/<relay-password>${ICECAST_RELAY_PASSWORD}<\/relay-password>/g" /etc/icecast2/icecast.xml
+sed -i "s/<admin-password>[^<]*<\/admin-password>/<admin-password>${ICECAST_ADMIN_PASSWORD}<\/admin-password>/g" /etc/icecast2/icecast.xml
 
 # Pokrećemo Icecast server
 exec sudo -Eu icecast2 icecast2 -n -c /etc/icecast2/icecast.xml
