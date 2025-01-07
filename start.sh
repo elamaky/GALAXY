556e2572b5d6ca353f33357a971c24dceebe2345
elamaky <nemirnaela@gmail.com>
2025-01-07 08:28:46 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,24 +1,10 @@
-#!/bin/sh
+#!/bin/bash
 
-env
+# Ažuriramo konfiguracioni fajl sa odgovarajućim vrednostima
+sed -i "s/<hostname>[^<]*<\/hostname>/<hostname>${ICECAST_HOSTNAME}<\/hostname>/g" /etc/icecast2/icecast.xml
+sed -i "s/<source-password>[^<]*<\/source-password>/<source-password>galaxy<\/source-password>/g" /etc/icecast2/icecast.xml
+sed -i "s/<relay-password>[^<]*<\/relay-password>/<relay-password>galaxy<\/relay-password>/g" /etc/icecast2/icecast.xml
+sed -i "s/<admin-password>[^<]*<\/admin-password>/<admin-password>galaxy<\/admin-password>/g" /etc/icecast2/icecast.xml
 
-set -x
-
-set_val() {
-    if [ -n "$2" ]; then
-        echo "set '$2' to '$1'"
-        sed -i "s/<$2>[^<]*<\/$2>/<$2>$1<\/$2>/g" /etc/icecast2/icecast.xml
-    else
-        echo "Setting for '$1' is missing, skipping." >&2
-    fi
-}
-
-set_val $ICECAST_SOURCE_PASSWORD source-password
-set_val $ICECAST_RELAY_PASSWORD  relay-password
-set_val $ICECAST_ADMIN_PASSWORD  admin-password
-set_val $ICECAST_PASSWORD        password
-set_val $ICECAST_HOSTNAME        hostname
-
-set -e
-
-sudo -Eu icecast2 icecast2 -n -c /etc/icecast2/icecast.xml
+# Pokrećemo Icecast server
+exec sudo -Eu icecast2 icecast2 -n -c /etc/icecast2/icecast.xml
